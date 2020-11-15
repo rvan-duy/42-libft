db239d69b628c2a34066fb53bd02f1ba13bd35bf
Ruben Van duyneveldt <rvan-duy@f1r1s13.codam.nl>
2020-11-15 15:05:41 +0100

    finally fixed memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/07 17:34:05 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/14 17:47:24 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/15 14:22:11 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,16 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	size_t	i;
-	unsigned char	*destination;
-	unsigned char	*source;
-
-	if (!dst || !src)
-		return (NULL);
 	if (!n)
-		return(dst);
-	i = 0;
-	destination = (unsigned char *)dst;
-	source = (unsigned char *)src;
-	if (source > destination)
+		return (dst);
+	if (src > dst)
+		ft_memcpy(dst, src, n);
+	if (src < dst)
 	{
-		while (i < n)
+		while (n > 0)
 		{
-			destination[i] = source[i];
-			i++;
-		}
-	}
-	else
-	{
-		i = n;
-		while (i >= 0)
-		{
-			destination[i] = source[i];
-			i--;
+			((unsigned char *)dst)[n - 1] = ((unsigned char *)src)[n - 1];
+			n--;
 		}
 	}
 	return (dst);
