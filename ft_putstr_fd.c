0e5438d15f08c9417aef1ab843fecf541221933e
rvan-duy <rvan-duy@student.codam.nl>
2020-11-16 08:28:02 +0100

    cleaned up code

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -17,12 +17,11 @@ void	ft_putstr_fd(char *s, int fd)
 	int i;
 
 	i = 0;
-	if (fd && s)
+	if (!s)
+		return ;
+	while (s[i] != '\0')
 	{
-		while (s[i] != '\0')
-		{
-			ft_putchar_fd(s[i], fd);
-			i++;
-		}
+		ft_putchar_fd(s[i], fd);
+		i++;
 	}
 }
