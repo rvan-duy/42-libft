2b0fb459fb81723a2d109446548e69691cdb361c
ruben <rvan-duy@codam.nl>
2020-11-17 12:22:50 +0100

    reverted changes

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -15,17 +15,26 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*newstr;
-	int		s1len;
-	int		s2len;
-	
-	s1len = ft_strlen(s1);
-	s2len = ft_strlen(s2);
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	newstr = ft_calloc(s1len + s2len + 1, sizeof(char));
+	newstr = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!newstr)
 		return (NULL);
-	ft_strlcpy(newstr, s1, s1len);
-	ft_strlcat(newstr, s2, s2len + 1);
+	while (s1[i] != '\0')
+	{
+		newstr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		newstr[i] = s2[j];
+		i++;
+		j++;
+	}
 	return (newstr);
 }
