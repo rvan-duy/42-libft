be98c472a714746a476700898458a0880d2bc76a
ruben <rvan-duy@codam.nl>
2020-11-18 13:19:44 +0100

    initial commit

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstnew_bonus.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/18 13:18:19 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2020/11/18 13:18:39 by rvan-duy      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *list;
+
+	list = malloc(sizeof(t_list));
+	if (!list)
+		return (0);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
