a2878451cfe0bbca2aa698829d5f16e89c594713
rvan-duy <rvan-duy@student.codam.nl>
2020-11-18 13:01:03 +0100

    initial commit

---
+++ ft_lstadd_front.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstadd_front.c                                  :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/18 11:54:04 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2020/11/18 12:05:51 by rvan-duy      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_lstadd_front(t_list **lst, t_list *new)
+{
+    if (!lst || !new)
+        return ;
+    new->next = *lst;
+    *lst = new;
+}
