5ec7a58a37dd3a2ccd71feed5468ed5648d331a5
ruben <rvan-duy@codam.nl>
2020-11-18 16:14:19 +0100

    initial commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/31 17:44:08 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/18 13:47:40 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/18 14:37:52 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,7 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_listsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
