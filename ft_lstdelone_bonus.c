c1549db564557f9acb3ff44963645082077fa1fa
Ruben Van duyneveldt <rvan-duy@f0r6s8.codam.nl>
2020-11-19 17:16:50 +0100

    initial commit

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lstdelone_bonus.c                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/19 16:17:11 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2020/11/19 16:45:41 by rvan-duy      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
