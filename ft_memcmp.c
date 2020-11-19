83d5f89df84d5040c39231cf949434f04400ef20
rvan-duy <rvan-duy@student.codam.nl>
2020-11-19 21:24:24 +0100

    shortened code

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: ruben <rvan-duy@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/03 10:19:39 by ruben         #+#    #+#                 */
-/*   Updated: 2020/11/07 14:20:51 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/19 21:19:57 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 
 int	ft_memcmp(const void *str1, const void *str2, size_t n)
 {
-	unsigned char	*s1;
-	unsigned char	*s2;
-	size_t			i;
+	size_t	i;
 
-	s1 = (unsigned char *)str1;
-	s2 = (unsigned char *)str2;
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (((unsigned char*)str1)[i] != ((unsigned char*)str2)[i])
+			return (((unsigned char*)str1)[i] - ((unsigned char*)str2)[i]);
 		i++;
 	}
 	return (0);
