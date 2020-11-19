5863ca6a6af71b8b88f646257214033670d5392f
Ruben Van duyneveldt <rvan-duy@f0r6s8.codam.nl>
2020-11-19 12:39:49 +0100

    function stops correctly now

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/18 14:32:40 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/18 14:36:08 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/19 11:23:53 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	if (!lst)
 		return (NULL);
-	while (lst)
+	while (lst->next != NULL)
 		lst = lst->next;
 	return (lst);
 }
