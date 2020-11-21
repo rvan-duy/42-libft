2b7b72ec34e13e1a2779d8d90b040fc13e92b20c
Ruben Van duyneveldt <rvan-duy@f1r1s13.codam.nl>
2020-11-21 10:35:41 +0100

    added function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/31 17:44:08 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/19 12:36:18 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2020/11/21 10:35:05 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,5 +62,7 @@ void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
