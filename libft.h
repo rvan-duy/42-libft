bfe227f731ac79255e1fd35d3c0219d578a846e0
ruben <rvan-duy@codam.nl>
2021-01-08 19:38:50 +0100

    added strndup

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/31 17:44:08 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2020/11/23 12:02:06 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/01/08 19:33:59 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,5 +67,11 @@ void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 					void (*del)(void *));
+
+/*
+**	ADDED FUNCTIONS
+*/
+
+char				*ft_strndup(const char *s, size_t n);
 
 #endif
