164ae6693692c541af8890224b5a1db9357fd8a9
ruben <rvan-duy@codam.nl>
2021-06-20 15:26:58 +0200

    segfault fix

--- ft_array_str_duplicate.c
+++ ft_array_str_duplicate.c
@@ -1,23 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_array_free.c                                    :+:    :+:            */
+/*   ft_array_str_duplicate.c                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2021/06/20 14:12:48 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/06/20 15:25:30 by rvan-duy      ########   odam.nl         */
+/*   Created: 2021/06/20 15:08:08 by rvan-duy      #+#    #+#                 */
+/*   Updated: 2021/06/20 15:21:11 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_array_free(void **array, size_t len)
+char	**ft_array_str_duplicate(char **array, size_t array_len)
 {
-	while (len > 0)
+	char	**new_array;
+	size_t	i;
+
+	new_array = ft_calloc(array_len, sizeof(char *));
+	if (new_array == NULL)
+		return (NULL);
+	i = 0;
+	while (i < array_len)
 	{
-		len--;
-		ft_free(&array[len]);
+		new_array[i] = ft_strdup(array[i]);
+		if (new_array == NULL)
+		{
+			while (i > 0)
+			{
+				i--;
+				ft_free(&new_array[i]);
+			}
+			ft_free(&new_array);
+			return (NULL);
+		}
+		i++;
 	}
-	ft_free(&array);
+	return (new_array);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/31 17:44:08 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/06/20 14:12:10 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/06/20 15:17:29 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,7 @@ void	ft_error_and_exit(int ret, char *msg);
 void	ft_close_fd(int fd);
 size_t	ft_array_len(void **array);
 void	ft_array_free(void **array, size_t len);
+char	**ft_array_str_duplicate(char **array, size_t array_len);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 128
