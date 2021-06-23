ef35d1a13b8f63b3d7cbde565787d49db38b2d00
ruben <rvan-duy@codam.nl>
2021-06-23 21:01:01 +0200

    protect malloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/01 14:25:23 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/06/23 09:12:55 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/06/23 21:00:36 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	*ft_realloc(void *ptr, size_t count, size_t size)
 
 	newptr = ft_calloc(count, size);
 	if (newptr == NULL)
+	{
+		ft_free(&ptr);
 		return (NULL);
+	}
 	ft_memcpy(newptr, ptr, count);
 	ft_free(&ptr);
 	return (newptr);
